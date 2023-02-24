e80d4ca4bee0c2399fb2a9b669decf05f0d55070
mirzacoder1 <mirzazabbar654@gmail.com>
2023-02-24 08:00:42 +0530

    day10 Completed

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -10,6 +10,7 @@ class MyTheme{
           color:Colors.white,
           elevation:0.0,
           iconTheme:IconThemeData(color: Colors.black),
+          // ignore: deprecated_member_use
           textTheme:Theme.of(context).textTheme,
         ),);
   static ThemeData DarkTheme(BuildContext context)=>ThemeData(
