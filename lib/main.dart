f3163ab75d22e893a1a27e44901df08a4b8c69d0
mirzacoder1 <mirzazabbar654@gmail.com>
2023-02-24 07:59:14 +0530

    Day10 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:myfirst_app/pages/home_page.dart';
 import 'package:myfirst_app/pages/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myfirst_app/utils/routes.dart';
+import 'package:myfirst_app/widgets/themes.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -12,15 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.dark,
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        primaryTextTheme: GoogleFonts.latoTextTheme()
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.light,
-      ),
+      themeMode: ThemeMode.light,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.DarkTheme(context),
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       // initialRoute: MyRoutes.loginRoute,
