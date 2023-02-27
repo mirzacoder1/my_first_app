ac4c8ae980cb4743669d264a5ad1dea9b488d370
mirzacoder1 <mirzazabbar654@gmail.com>
2023-02-27 13:39:35 +0530

    Day 16 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,20 +20,19 @@ class _HomePageState extends State<HomePage> {
   final announce = "Welcome to ";
   final ment = " days of Flutter";
 
-
   @override
   void initState() {
     super.initState();
     loadData();
   }
+
   loadData() async {
-    await Future.delayed(Duration(seconds:3));
+    await Future.delayed(Duration(seconds: 3));
     var cataalogJson = await rootBundle.loadString("assets/files/catalog.json");
     var decodeData = jsonDecode(cataalogJson);
     var productsData = decodeData["products"];
-    CatalogModel.items = List.from(productsData)
-        .map((item) => Item.fromMap(item))
-        .toList();
+    CatalogModel.items =
+        List.from(productsData).map((item) => Item.fromMap(item)).toList();
     setState(() {});
   }
 
@@ -41,22 +40,28 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     var scaffold = Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App..."),
+        title: Center(
+          child: Text("Catalog App...",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),),
+        ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        // ignore: unnecessary_null_comparison
-        child:(CatalogModel.items!=null && CatalogModel.items.isNotEmpty)?ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: CatalogModel.items[index],
-            );
-          },
-        ): Center(
-          child: CircularProgressIndicator(),
-        )
-      ),
+          padding: const EdgeInsets.all(16.0),
+          // ignore: unnecessary_null_comparison
+          child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+              ? ListView.builder(
+                  itemCount: CatalogModel.items.length,
+                  itemBuilder: (context, index) {
+                    return ItemWidget(
+                      item: CatalogModel.items[index],
+                    );
+                  },
+                )
+              : Center(
+                  child: CircularProgressIndicator(),
+                )),
       drawer: MyDrawer(),
     );
     return scaffold;
